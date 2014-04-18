a7be7cfe7216460cb3d73e043811a2dca543a989
Samuel Liu <samuelliu@Samuels-MacBook-Pro.local>
2014-04-18 15:20:32 +0800

    init upload

---
+++ XYXMPPServiceDemo/XYXMPPServiceDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XYXMPPServiceDemo
+//
+//  Created by Samuel Liu on 4/18/14.
+//  Copyright (c) 2014 TelenavSoftware Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ XYXMPPServiceDemo/XYXMPPServiceDemoTests/XYXMPPServiceDemoTests.m
@@ -0,0 +1,34 @@
+//
+//  XYXMPPServiceDemoTests.m
+//  XYXMPPServiceDemoTests
+//
+//  Created by Samuel Liu on 4/18/14.
+//  Copyright (c) 2014 TelenavSoftware Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface XYXMPPServiceDemoTests : XCTestCase
+
+@end
+
+@implementation XYXMPPServiceDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
